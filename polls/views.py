750ea05aac0e3e6bccef6d4a57eb23932e4ea3f0
PanidaOun <panida.ou@ku.th>
2020-09-17 23:56:07 +0700

    Redirect users to the polls index page

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
 from .models import Choice, Question
+from django.contrib import messages
 
 
 class IndexView(generic.ListView):
@@ -20,16 +21,23 @@ class IndexView(generic.ListView):
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
 
+def view_vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if not question.can_vote():
+        messages.error(request, f"Voting is not allowed")
+        return redirect('polls:index')
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
+    return render(request,'polls/detail.html',{'question':question})
+
+# class DetailView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/detail.html'
     
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+#     def get_queryset(self):
+#         """
+#         Excludes any questions that aren't published yet.
+#         """
+#         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
